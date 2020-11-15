31b582e759f8a0816a2586e427cefb9e13c310c2
Iryna Prendkovych <iryna.prendkovych.itis.2020@lpnu.ua>
2020-11-15 18:02:32 +0200

    lab 6.2

---
+++ лаб 6,2/UnitTest6.2/UnitTest6.2.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаб 6,2/лаб 6,2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62
+{
+	TEST_CLASS(UnitTest62)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			const int n = 6;
+			int a[n]{ 10, 4, 7, 3, 0, 1 };
+			t = Min(a, n);
+			Assert::AreEqual(t, 4);
+		}
+	};
+}
